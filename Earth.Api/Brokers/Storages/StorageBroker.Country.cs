e4c9bdd8a2755c45f97d2f64fe091612231861af
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2024-10-23 00:29:45 +0500

    DATA: Model & Migrate Countries

---
+++ Earth.Api/Brokers/Storages/StorageBroker.Country.cs
@@ -0,0 +1,14 @@
+﻿//--------------------------------------------------
+// Copyright (c) Coalition Of Good-Hearted Engineers
+// Free To Use To Find Comfort And Peace
+//--------------------------------------------------
+using Earth.Api.Models.Foundations.Countries;
+using Microsoft.EntityFrameworkCore;
+
+namespace Earth.Api.Brokers.Storages
+{
+    public partial class StorageBroker
+    {
+        public DbSet<Country> Countries { get; set; }
+    }
+}
---
+++ Earth.Api/Models/Foundations/Countries/Country.cs
@@ -0,0 +1,14 @@
+﻿//--------------------------------------------------
+// Copyright (c) Coalition Of Good-Hearted Engineers
+// Free To Use To Find Comfort And Peace
+//--------------------------------------------------
+namespace Earth.Api.Models.Foundations.Countries
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } 
+        public string Code { get; set; } 
+        public string Region { get; set; }
+    }
+}
